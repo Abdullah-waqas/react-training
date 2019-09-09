d244097451b0deab7c38bb1c0df288ac39ee16e7
Abdullah Waqas <dev.waqas08@gmail.com>
2019-09-09 22:10:40 +0500

    Added userlist component

--- Lecture-2/index.js
+++ Lecture-2/index.js
@@ -1,1 +1,41 @@
+class UserList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    }
+  }
 
+  componentDidMount() {
+    fetchAPI().then(({ data }) => {
+      this.setState({
+        users: data,
+      })
+    })
+  }
+  render() {
+    return (
+      <div>
+        {this.state.users.map(user => <User {...user}/>)}
+      </div>
+    );
+  }
+}
+
+const User = ({
+  id,
+  first_name,
+  last_name,
+  email,
+  avatar,
+}) => {
+  return (
+    <div>
+      <p>ID: {id}</p>
+      <p>First Name: {first_name}</p>
+      <p>Last Name: {last_name}</p>
+      <p>Email: {email}</p>
+      <img src={avatar}/>
+    </div>
+  );
+}
