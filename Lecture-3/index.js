eddcc69dbac8f18f923741a7a5b9672d340ce7e2
Abdullah Waqas <dev.waqas08@gmail.com>
2019-09-10 21:16:50 +0500

    Added router.js

--- Lecture-3/index.js
+++ Lecture-3/index.js
@@ -1,1 +1,67 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+
+class Contact extends React.Component {
+  render() {
+    return <h1>Contact</h1>
+  }
+}
+
+class Notfound extends React.Component {
+  render() {
+    return (
+      <h1>Page Not Found</h1>
+    )
+  }
+}
+
+class Users extends React.Component {
+  render() {
+    console.log(this.props);
+    return <h1>Users</h1>
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+      </div>
+    )
+  }
+}
+
+
+const Routing = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Notfound} />
+      </Switch>
+    </div>
+  </Router>
+)
+
+
+
+
+ReactDOM.render(<Routing />, document.getElementById('root'));
